8c1bb83d41e971aee743dd5f6c36ee4058fa19b6
ninjada <jada.white@stackla.com>
2015-02-12 10:47:35 +1100

    server types

--- install.sh
+++ install.sh
@@ -3,6 +3,14 @@
 # By NinjadaWhite jadawhite@gmail.com
 # Download + Install logstash-forwarder rpm
 
+# Server Config Types
+srvVarn='cat /home/ec2-user/logstash-forwarder/cfg/prod-varnish.cfg'
+srvCont='cat /home/ec2-user/logstash-forwarder/cfg/prod-content.cfg'
+srvData='cat /home/ec2-user/logstash-forwarder/cfg/prod-data-service.cfg'
+srvWeb='cat /home/ec2-user/logstash-forwarder/cfg/prod-web-eph.cfg'
+srvWebApi='cat /home/ec2-user/logstash-forwarder/cfg/prod-web-api-eph.cfg'
+
+# Install  logstash-forwarder
 cd /home/ec2-user/logtash-forwarder;
 sudo curl -O http://ninjada.com/logstash/logstash-forwarder-0.3.1-1.x86_64.rpm;
 sudo rpm -ivh /home/ec2-user/logstash-forwarder/logstash-forwarder-0.3.1-1.x86_64.rpm;
@@ -17,7 +25,7 @@ sudo cp /home/ec2-user/logstash-forwarder/logstash-forwarder1 /etc/sysconfig/log
 
 # Create and configure /etc/logstash-forwarder 
 cd /home/ec2-user/logstash-forwarder;
-sudo cp /home/ec2-user/logstash-forwarder/logstash-forwarder2 /etc/logstash-forwarder;
+sudo $srvVarn /etc/logstash-forwarder;
 
 # Chkconfig
 sudo chkconfig --add logstash-forwarder;

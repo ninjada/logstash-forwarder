b4bc1c13cfbf86e0968a599a7d317807200676e3
ninjada <ninjada@users.noreply.github.com>
2015-02-13 14:49:13 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,10 +15,6 @@ cd /home/ec2-user/logtash-forwarder;
 sudo curl -O http://ninjada.com/logstash/logstash-forwarder-0.3.1-1.x86_64.rpm;
 sudo rpm -ivh /home/ec2-user/logstash-forwarder/logstash-forwarder-0.3.1-1.x86_64.rpm;
 
-# Copy logstash forwarder init script 
-sudo curl -o /etc/init.d/logstash-forwarder http://ninjada.com/logstash/logstash_forwarder_redhat_init;
-sudo chmod +x /etc/init.d/logstash-forwarder;
-
 # Init depends on file at /etc/sysconfig/logstash-forwarder
 cd /home/ec2-user/logstash-forwarder;
 sudo cp /home/ec2-user/logstash-forwarder/logstash-forwarder1 /etc/sysconfig/logstash-forwarder;
@@ -29,6 +25,10 @@ sudo cp $srvVarn /etc/logstash-forwarder;
 # Copy Logrotate Files
 sudo cp logrotate.hourly /etc/cron.hourly/;
 sudo cp logrotate.hourly.conf /etc/;
+
+# Copy logstash forwarder init script 
+sudo cp /home/ec2-user/logstash-forwarder/logstash-forwarder-init /etc/init.d/logstash-forwarder
+sudo chmod +x /etc/init.d/logstash-forwarder;
 
 # Chkconfig
 sudo chkconfig --add logstash-forwarder;
